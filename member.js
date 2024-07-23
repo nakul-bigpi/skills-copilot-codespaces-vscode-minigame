a628002438999949fe6ace8c60196c124de1ded5
Nakul Sharma <110083584+nakul-bigpi@users.noreply.github.com>
2024-07-23 05:11:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML',
+            level: 'Expert'
+        },
+        {
+            name: 'CSS',
+            level: 'Expert'
+        },
+        {
+            name: 'JavaScript',
+            level: 'Expert'
+        },
+        {
+            name: 'React',
+            level: 'Expert'
+        },
+        {
+            name: 'Angular',
+            level: 'Expert'
+        },
+        {
+            name: 'Vue',
+            level: 'Expert'
+        }
+    ];
+    return skills;
+}
